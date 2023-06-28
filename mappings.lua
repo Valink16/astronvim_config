7a181077a8fbc4d3fde2a6913440909fbcda8862
Valink16 <valuthy.karunakaran@epfl.ch>
2023-06-29 01:26:14 +0200

    some adjustments

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,10 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
@@ -20,6 +23,12 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- Fix split resizing for MacOS
+    ["<C-S-k>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" },
+    ["<C-S-j>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" },
+    ["<C-S-h>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" },
+    ["<C-S-l>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" },
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,39 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    opts = {
+    },
+    event = "User AstroFile",
+    cmd = {
+      "TodoQuickFix",
+    },
+  },
+
+  "catppuccin/nvim",
+
+  {
+    "f-person/auto-dark-mode.nvim",
+    config = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option("background", "dark")
+        vim.cmd("colorscheme catppuccin-mocha")
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option("background", "light")
+        vim.cmd("colorscheme catppuccin-latte")
+      end
+    },
+    init = function()
+      require("auto-dark-mode").init()
+    end
+  }
+
+  -- TODO: Configure auto theme switching
 }
